410d3df48a0dc880769a17267217c292493ac252
Sinan Bayar <sinan@mudi.app>
2023-05-14 21:02:56 +0300

    Blog 2.Part Completed

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 4000;
+// const path = require("path");
 
 app.listen(port, () => {
   console.log(`Sunucu ${port} portunda başlatıldı...`);
@@ -12,6 +13,25 @@ const blog = {
   description: 'Blog description',
 };
 
+// ROUTES
 app.get('/', (req, res) => {
-  res.send(blog);
+  res.render("index")
 });
+
+app.get('/about', (req, res) => {
+  res.render("about")
+});
+
+app.get('/post', (req, res) => {
+  res.render("post")
+});
+
+app.get('/add_post', (req, res) => {
+  res.render("add_post")
+});
+
+// MIDDLEWARES
+app.use(express.static('public'));  // Statik dosyalarımızı public klasörümüz üzerinden çalıştırıyoruz.
+
+// TEMPLATE ENGINE
+app.set("view engine", "ejs");  // Uygulamamızda ejs modülünü kullanıyoruz.
